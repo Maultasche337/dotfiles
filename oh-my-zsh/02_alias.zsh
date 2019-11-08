5d3e13edb9a916842a2a216b2347d2f19ce62ed3
Chris Metz <me@chris-metz.dev>
2019-11-08 13:20:47 +0000

    add aliases for 'exa' (el + ela)

--- oh-my-zsh/02_alias.zsh
+++ oh-my-zsh/02_alias.zsh
@@ -1,6 +1,8 @@
 alias aptup="sudo apt update"
 alias aptug="sudo apt full-upgrade"
 alias dc="docker-compose"
+alias el="exa -lg --git"
+alias ela="exa -lga --git"
 alias gssm="git stash save -p"
 alias ports="sudo ss -ltnp"
 
