56d7ec4570a45ec7e84499df4c1cab7538a368ac
Chris Metz <me@chris-metz.dev>
2019-11-08 20:26:05 +0100

    install.sh - add function to install exa tool

--- install.sh
+++ install.sh
@@ -13,6 +13,16 @@ function install_env() {
   echo "Done."
 }
 
+function install_exa() {
+  echo "Installing exa"
+  cd /tmp/
+  wget https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip
+  sudo apt -y install unzip
+  unzip exa-linux-x86_64-0.9.0.zip
+  sudo cp exa-linux-x86_64 /usr/local/bin/exa
+  rm exa-linux-x86_64 exa-linux-x86_64-0.9.0.zip
+}
+
 function install_omz() {
   echo "Installing oh-my-zsh files"
   cp -v oh-my-zsh/* ~/.oh-my-zsh/custom/
@@ -32,6 +42,9 @@ case $1 in
   env)
     install_env
     ;;
+  exa)
+    install_exa
+    ;;
   omz)
     install_omz
     ;;
@@ -41,6 +54,7 @@ case $1 in
   *)
     echo "Available commands:"
     echo "env"
+    echo "exa - Install exa (ls replacement)"
     echo "omz - Install oh-my-zsh files"
     echo "vim"
     ;;
