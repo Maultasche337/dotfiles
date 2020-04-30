dc3fdd24081400578ec5a13606bc0daaaac42a93
Chris Metz <me@chris-metz.dev>
2020-04-30 19:30:31 +0200

    oh-my-zsh alias - exa group-directories-first

--- oh-my-zsh/02_alias.zsh
+++ oh-my-zsh/02_alias.zsh
@@ -1,8 +1,8 @@
 alias aptup="sudo apt update"
 alias aptug="sudo apt full-upgrade"
 alias dc="docker-compose"
-alias el="exa -lg --git"
-alias ela="exa -lga --git"
+alias el="exa -lg --git --group-directories-first"
+alias ela="exa -lga --git --group-directories-first"
 alias gssm="git stash save -p"
 alias ports="sudo ss -ltnp"
 
