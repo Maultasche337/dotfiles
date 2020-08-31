6a757be94a19b244b4a70bac3d5f144cb77f69c3
Chris Metz <me@chris-metz.dev>
2020-08-31 09:17:24 +0200

    oh my zsh - remove "dc" docker-compose alias (due to existing plugin)

--- oh-my-zsh/02_alias.zsh
+++ oh-my-zsh/02_alias.zsh
@@ -1,6 +1,6 @@
 alias aptup="sudo apt update"
 alias aptug="sudo apt full-upgrade"
-alias dc="docker-compose"
+#alias dc="docker-compose"
 alias el="exa -lg --git --group-directories-first"
 alias ela="exa -lga --git --group-directories-first"
 alias gssm="git stash save -p"
