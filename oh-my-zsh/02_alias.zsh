34aa7c6666bbe7fbe57b9f2ff2e8c90d0cbe37a9
Chris Metz <me@chris-metz.dev>
2020-09-18 12:51:14 +0200

    add omz alias df=df -h

--- oh-my-zsh/02_alias.zsh
+++ oh-my-zsh/02_alias.zsh
@@ -1,6 +1,6 @@
 alias aptup="sudo apt update"
 alias aptug="sudo apt full-upgrade"
-#alias dc="docker-compose"
+alias df="df -h"
 alias el="exa -lg --git --group-directories-first"
 alias ela="exa -lga --git --group-directories-first"
 alias gssm="git stash save -p"
