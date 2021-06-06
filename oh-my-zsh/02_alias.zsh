f0ffc057315ff97760f9b8a1a638107886584e51
Chris Metz <me@chris-metz.dev>
2021-06-06 08:16:58 +0000

    omz alias - change du to du1 (and ignore errors)

--- oh-my-zsh/02_alias.zsh
+++ oh-my-zsh/02_alias.zsh
@@ -1,7 +1,7 @@
 alias aptup="sudo apt update"
 alias aptug="sudo apt full-upgrade"
 alias df="df -h"
-alias du="du -h --max-depth=1"
+alias du1="du -h --max-depth=1 2>/dev/null"
 alias dps="docker ps -a"
 alias el="exa -lg --git --group-directories-first"
 alias ela="exa -lga --git --group-directories-first"
